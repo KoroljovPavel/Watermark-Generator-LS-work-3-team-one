10d7b6914d0608c034f11004908850e19ea45ea7
Dmitry Vyalkov <dima-mav@yandex.ua>
2016-01-07 11:05:21 +0200

    basic upload image backend module

--- app/php/config.php
+++ app/php/config.php
@@ -1,9 +1,5 @@
 <?php
 
-$__config = array(
-
-        'upload' => array(
-                'maxFileSize' => 20*1024*1024    
-            )
-
-    );
+$__config['upload']['maxFileSize'] = 20*1024*1024;
+$__config['path']['root'] = '../';
+$__config['path']['imgUpload'] = $__config['path']['root'] . '';
--- app/php/upload.php
+++ app/php/upload.php
@@ -5,7 +5,7 @@
 session_start();
 
 // Не указан тип файла
-if (!isset($_POST["filetype"])) {
+if (!isset($_GET["fileType"])) {
     echo json_encode( array(
         'status' => 'error',                 
         'errorType' => 'queryFormat',
@@ -16,9 +16,9 @@ if (!isset($_POST["filetype"])) {
 }
 
 // проверяем тип файла
-$fileType = htmlentities(trim($_POST["filetype"]), ENT_QUOTES, "UTF-8");
+$fileType = htmlentities(trim($_GET["fileType"]), ENT_QUOTES, "UTF-8");
 
-if ($fileType != "img" || $fileType != "watermark") {
+if (!($fileType == "img" || $fileType == "watermark")) {
     echo json_encode( array(
         'status' => 'error',                 
         'errorType' => 'queryFormat',
@@ -109,10 +109,69 @@ if (false === $ext = array_search(
         'errorType' => 'img',
         'errorData' => "Недопустимый формат файла"              
         ), JSON_FORCE_OBJECT
-    );     
+    ); 
+    exit();    
 }
 
-echo json_encode( array(
-        'status' => 'success'            
+// Копируем изображение
+
+$sId = session_id(); // Получаем идентификатор сессии для именования файлов
+
+// создаем путик файлам
+$imgs['orig']['name']  = $sId . "-$fileType." . $ext;
+$imgs['orig']['path']  = $__config["path"]["imgUpload"] . $imgs['orig']['name'];
+$imgs['min']['name']   = $sId . "-$fileType-min." . $ext;
+$imgs['min']['path']   = $__config["path"]["imgUpload"] . $imgs['min']['name'];
+
+if (!move_uploaded_file ( $_FILES[$fileType]['tmp_name'], $imgs['orig']['path'])) {
+    echo json_encode( array(
+        'status' => 'error',                 
+        'errorType' => 'file',
+        'errorData' => "Не удалось скопировать изображение: "
+                       . $imgs['orig']['path']              
         ), JSON_FORCE_OBJECT
-    );   
+    ); 
+    exit();   
+}
+
+// Получаем данные о размере изображения
+$fileInfo  = getimagesize($imgs['orig']['path']);
+echo "<pre>";
+print_r($fileInfo);
+echo "</pre>";
+// Создаем миниатюру изображения
+function imageresize($outfile,$infile,$neww,$newh,$quality,$mime) {
+    if ($mime == "image/jpeg") {
+        $im=imagecreatefromjpeg($infile);
+    } else {
+        $im=imagecreatefrompng($infile);
+    }
+    
+    $im1=imagecreatetruecolor($neww,$newh);
+    imagecopyresampled($im1,$im,0,0,0,0,$neww,$newh,imagesx($im),imagesy($im));
+
+    imagejpeg($im1,$outfile,$quality);
+    imagedestroy($im);
+    imagedestroy($im1);
+}
+
+
+if ($file_info["width"] != 163 || $file_info["height"] != 119 || $file_info["mime"] != "image/jpeg") {
+    // Конвертируем изображение
+    //imageresize($_SESSION["img"],$_SESSION["img"],163,119,75,$file_info["mime"]);
+}
+
+// заглушка формирования миниатюры
+copy($imgs['orig']['path'], $imgs['min']['path']); 
+// Формируем ответ
+
+echo json_encode( array(
+        'status' => 'success',
+        'imgName' => $imgs['orig']['name'],
+        'minName' => $imgs['min']['name'],
+        'imgSize' => array (
+                'width' => $fileInfo[0],
+                'height' => $fileInfo[1]
+            )            
+        ), JSON_FORCE_OBJECT
+    );
