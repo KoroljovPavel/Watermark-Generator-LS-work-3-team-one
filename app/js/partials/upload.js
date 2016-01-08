3d49e533c23214faa3eee3b3d4c8460d4f86d86a
Vlad <like_mike@ukr.net>
2016-01-08 22:25:24 +0200

    add image display

--- app/js/partials/upload.js
+++ app/js/partials/upload.js
@@ -22,14 +22,24 @@ var upload = function() {
 			url: url,
 			dataType: 'json',
 			add: function (e, data) {
+				// Выводим название файла
 				$(this).parent().find('input').attr('placeholder', data.files[0].name);
+				console.log(data);
 	        	data.submit();
 	        },
 			// Получаем ответ от сервера
 			done: function (e, data) {
+				var path;
 	        	if (data.textStatus == 'success') {
 	        		console.log('Successfully uploaded');
 	        		console.log(data.result);
+	        		// Проверка (картинка или водяной знак)
+	        		if (data.result.minName.indexOf('-img') + 1) {
+	        			// Если картинка - определяем путь к ней
+	        			path = 'users_img/' + data.result.minName + '?' + event.timeStamp;
+	        			// Добавляем путь соответствующему элементу
+	        			$('.img-display').attr({'src':path, 'alt':'Ваша картинка'});
+	        		};
 	        	} else {
 	        		console.log('Upload error');
 	        		console.log(data.result);
