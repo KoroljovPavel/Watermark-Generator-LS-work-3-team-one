7660da97217e788d9af8fdcbc3e3d77a53818fed
lisit-sa <kristaliska@gmail.com>
2016-01-10 20:53:33 +0400

    add view buttons

--- app/js/partials/view-position.js
+++ app/js/partials/view-position.js
@@ -2,12 +2,16 @@ $(function() {
 		$('.view__item-one').click(function(event){
 			event.preventDefault();
 			console.log('первый вид')
+			$('.view__item-one').addClass('active');
+			$('.view__item-two').removeClass('active');
       		$('.manipulation__item_position-block--lines').show();
      		$('.manipulation__item_position-block--cells').hide();
     });
 		$('.view__item-two').click(function(event){
 			event.preventDefault();
 			console.log('второй вид')
+			$('.view__item-two').addClass('active');
+			$('.view__item-one').removeClass('active');
 			$('.manipulation__item_position-block--cells').show();
 			$('.manipulation__item_position-block--lines').hide();
 		});
