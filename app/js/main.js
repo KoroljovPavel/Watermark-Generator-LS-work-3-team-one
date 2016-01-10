dac9a7f8d33195de1d813de7f0fe0911d13b3819
lisit-sa <kristaliska@gmail.com>
2016-01-10 19:37:19 +0400

    main.js

---
+++ app/js/main.js
@@ -0,0 +1,19 @@
+//= ../bower/jquery/dist/jquery.min.js
+
+//= jquery.ui.js
+
+//= ../bower/blueimp-file-upload/js/vendor/jquery.ui.widget.js
+
+//= ../bower/blueimp-file-upload/js/jquery.iframe-transport.js
+
+//= ../bower/blueimp-file-upload/js/jquery.fileupload.js
+
+//= partials/watermark.js
+
+//= partials/upload.js
+
+//= ./partials/download.js
+
+//= partials/movement.js
+
+//= partials/view-position.js
